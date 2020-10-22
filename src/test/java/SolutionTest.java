b408a5e48395a39d1567657895a00207b16b8ee5
장민종 <alswhddl10@naver.com>
2020-10-22 22:31:21 +0900

    오류 수정

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -11,7 +11,8 @@ public class SolutionTest {
 
         assertEquals(3, sol.solution(new int[]{3,0,6,1,5}));
         assertEquals(6, sol.solution(new int[]{1545, 2, 999, 790, 540, 10, 22}));
-        assertEquals(3, sol.solution(new int[]{1, 7, 0, 1, 6, 4}));
+        assertEquals(4, sol.solution(new int[]{10, 8, 5, 4}));
+        assertEquals(3, sol.solution(new int[]{25, 8, 5, 3, 3}));
         assertEquals(0, sol.solution(new int[]{0}));
 
     }
